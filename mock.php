5eb645645dcf8c32e464308e2350b1836b895520
Dennis Lehmann <d.lehmann@optadata-gruppe.de>
2018-06-19 14:25:39 +0200

    add cors headers

--- mock.php
+++ mock.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: '.$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']);
 
 session_start();
 
