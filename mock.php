a330923890595d2ebd8aab86278ebdd611270cf5
Dennis Lehmann <d.lehmann@optadata-gruppe.de>
2019-09-20 14:46:02 +0200

    improved dictionary for api

--- mock.php
+++ mock.php
@@ -1,30 +1,68 @@
 <?php
-header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+
+//Cross-origin permissions, access from remote
+header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Headers: '.$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']);
+header('Access-Control-Allow-Headers: ' . $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']);
 
+//Starting session for cookie generation
 session_start();
 
-$word = array('H', 'A', 'N', 'G', 'M', 'A', 'N');
-$found = array(null, null, null, null, null, null, null);
+//Array with words for the game
+$dict = array(
+    "Auto",
+    "Haus",
+    "Kirche",
+    "Vertrag",
+    "Betriebssystem",
+    "Lampenschirm",
+    "Monitor",
+    "Praktikum",
+    "Wolke",
+    "Führungskraft",
+    "Schwimmbad",
+    "Datenbank",
+    "Präsentation",
+    "Ernährung",
+    "Ketogen",
+    "Telefonkonferenz",
+    "Mediengestalter",
+    "Kantine",
+    "Zimmermann",
+    "Armbanduhr",
+    "Software",
+    "Hardware",
+    "Entwicklungsumgebung",
+    "Fenster",
+    "Terminal",
+    "Performanz",
+    "Animation",
+    "Ladebalken",
+    "Klasse",
+    "Meisterprüfung"
+);
 
-
+//Requestobject from client
 $request = json_decode(file_get_contents('php://input'), true);
 
+//Method for starting the game
 if ($_GET['api'] === 'startgame') {
+    $_SESSION['selectedWord'] = str_split(strtoupper($dict[rand(0, 29)]));
+    $_SESSION['foundWord'] = array_fill(0, count($_SESSION['selectedWord']), null);
     $_SESSION['status'] = array(
-        'word' => $found,
+        'word' => $_SESSION['foundWord'],
         'status' => 'PLAYING',
         'failedAttempts' => 0
     );
 }
 
+//Method for playing the game
 if ($_GET['api'] === 'game') {
     $foundChar = false;
-    for ($i = 0; $i < count($word); $i++) {
-        if ($word[$i] === strtoupper($request)) {
-            $_SESSION['status']['word'][$i] = $word[$i];
+    for ($i = 0; $i < count($_SESSION['selectedWord']); $i++) {
+        if ($_SESSION['selectedWord'][$i] === strtoupper($request)) {
+            $_SESSION['status']['word'][$i] = $_SESSION['selectedWord'][$i];
             $foundChar = true;
         }
     }
@@ -32,21 +70,16 @@ if ($_GET['api'] === 'game') {
         $_SESSION['status']['failedAttempts']++;
     }
     if ($_SESSION['status']['failedAttempts'] === 10) {
-        $_SESSION['status']['word'] = $word;
+        $_SESSION['status']['word'] = $_SESSION['selectedWord'];
         $_SESSION['status']['status'] = 'LOSE';
-    } else if (implode($_SESSION['status']['word']) === implode($word)) {
+    } else if (implode($_SESSION['status']['word']) === implode($_SESSION['selectedWord'])) {
         $_SESSION['status']['status'] = 'WIN';
     }
 }
 
+//Return of the JSON for playing
 if (isset($_SESSION['status'])) {
     echo json_encode($_SESSION['status']);
     exit;
 }
-?><pre>
-No game status found. Use as follows:
-
-    /mock.php?api=startgame   // startgame api
-    /mock.php?api=game        // game api
-
-
+?>
